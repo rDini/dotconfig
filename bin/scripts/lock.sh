8e4d03a91275181d033ed46f2058f5f409e7cdab
rDini <rDini@users.noreply.github.com>
2019-05-27 17:17:33 -0300

    Support to multiple monitors
    Support to multiple monitors

--- bin/scripts/lock.sh
+++ bin/scripts/lock.sh
@@ -11,8 +11,13 @@ for output in $(swaymsg -t get_outputs | jq -r '.[] .name'); do
 done
 
 # printf '%s\n' "${images[@]}" | xargs -P 0 -I{} convert -blur 0x8 {} {}
-corrupter -mag 2 -boffset 35 ${images[@]} ${images[@]}
+# corrupter -mag 2 -boffset 35 ${images[@]} ${images[@]}
+# to work with multiple monitors
 
+for i in "${images[@]}";
+        do
+                /usr/local/bin/corrupter -mag 1 -boffset 25 "$i" "$i"
+        done
 
 swaylock ${swaylock_args[@]} -s center
 rm ${images[@]}
